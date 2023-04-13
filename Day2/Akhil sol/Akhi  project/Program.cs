2dde4e092105fc1ab8d4cb1211b8eb39a5b5e618
Akhil Kannuri <87297978+akhinani@users.noreply.github.com>
2023-04-13 17:57:20 +0530

    Add files via upload

---
+++ Day2/Akhil sol/Akhi  project/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Akhilproject
+{
+    class A
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, Akhil!");
+        }
+
+    }
+}
+
---
+++ Day2/Area circle/Code Circle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = 0; double a;
+            Console.WriteLine("enter radius");
+            r = double.Parse(Console.ReadLine());
+            a = Math.PI * r * r;
+            Console.WriteLine(a);
+
+        }
+        
+    }
+}
