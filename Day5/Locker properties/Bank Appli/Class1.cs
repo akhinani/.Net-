69873f114fa55d98918c9cf991e8328229d50f33
Akhil Kannuri <87297978+akhinani@users.noreply.github.com>
2023-04-13 18:01:23 +0530

    Add files via upload

---
+++ Day5/Locker properties/Bank Appli/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locker
+{
+    public class Locker
+    {
+        private int _number;
+        private string _locktype;
+        private string _password;
+        private double _amount;
+        private DateTime _lastOpened;
+
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+        public string Locktype
+        {
+            get { return _locktype; }
+            set { _locktype = value; }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        public double Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+        public DateTime LastOpened
+        {
+            get()
+        }
+        public Locker()
+        {
+            _number = 001;
+            _locktype = "A";
+            _password = "password";
+            _amount = 1000;
+            _lastOpened = DateTime.Now;
+
+        }
+        public Locker(int _number, string _locktype, string _password, double _amount, DateTime _lastOpened)
+
+        {
+            this._number = _number;
+            this._locktype = _locktype;
+            this._password = _password;
+            this._amount = _amount;
+            this._lastOpened = _lastOpened;
+
+        }
+
+    }
+
+}
+
---
+++ Day5/Notes/Find notes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Find_notes
+{
+    public class Program
+    {
+        public  void countCurrency(int amount)
+        {
+            int[] notes = new int[] { 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 1 };
+            int[] noteCounter = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    noteCounter[i] = amount / notes[i];
+                    amount = amount % notes[i];
+                }
+            }
+            Console.WriteLine("Currency Count ->");
+            for (int i = 0; i < 10; i++)
+            {
+           
+            }
+        }
+    }
+}
