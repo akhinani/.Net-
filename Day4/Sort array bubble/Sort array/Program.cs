849e6e7a89a5842cb21b09bc9dfe71d2aa4a70f8
Akhil Kannuri <87297978+akhinani@users.noreply.github.com>
2023-04-13 17:59:48 +0530

    Add files via upload

---
+++ Day4/Sort array bubble/Sort array/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace sortarray
+
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, temp;
+            int[] arr = new int[5];
+            Console.WriteLine("Enter 5 numbers: ");
+            for (i = 0; i < arr.Length; i++)
+            { 
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (i = 0; i < arr.Length; i++)
+            {
+                for (j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+
+                }
+
+            }
+
+            for (i = 0; i < arr.Length; i++)
+
+            {
+                Console.WriteLine();
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+
+    }
+
+}
+
---
+++ Day4/vowels and conso sol/vowels an cons/Program.cs
@@ -0,0 +1,48 @@
+﻿//Write a program in C# Sharp to count a total number of vowel or consonant in a string.
+
+using System;
+
+using System.Collections.Generic;
+
+using System.Diagnostics.Eventing.Reader;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace vowcon
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int i, vow = 0, cons = 0, len;
+            Console.WriteLine("Enter a String: ");
+            str = Console.ReadLine();
+            //len = str.Length;
+            for (i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+
+                  str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+                {
+                    vow++;
+                }
+                else if ((str[i] >= 'a') && (str[i] <= 'z') || (str[i] >= 'A') && (str[i] <= 'Z'))
+                {
+                    cons++;
+                }
+
+            }
+            Console.WriteLine("The number of vowels: {0}", vow);
+            Console.WriteLine("The number of consonants: {0}", cons);
+        }
+
+    }
+
+}
+
