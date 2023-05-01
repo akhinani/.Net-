6ea0cafcddad41adfbc32f598b4a353bd8ec88d3
Akhil Kannuri <87297978+akhinani@users.noreply.github.com>
2023-05-01 20:27:02 +0530

    Add files via upload

---
+++ Day16/SubArr with Given sum/SubArr with Given sum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SubArr_With_Given_sum
+
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, sum;
+            Console.WriteLine("Enter length of N:");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Sum:");
+            sum = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter your array elements:" );
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            subarray(n, sum,arr);
+        }
+        public static void subarray(int n, int sum, int[] arr)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int currentSum = arr[i];
+                if (currentSum == sum)
+                {
+                    
+                    return;
+                }
+                else
+                {
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        currentSum += arr[j];
+                        if (currentSum == sum)
+                        {
+                            Console.WriteLine("Sum found between indexes " + i + " and " + j);
+                            return;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("No subarray found");
+        }
+    }
+
+}
