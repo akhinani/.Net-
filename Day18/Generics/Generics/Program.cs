153e3fdcdafaf6f493d8559f647ac7ea1fbe3ca3
Akhil Kannuri <87297978+akhinani@users.noreply.github.com>
2023-05-01 20:28:00 +0530

    Add files via upload

---
+++ Day18/Generics/Generics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList s = new SortedList();
+            s.Add("Maharastra ", "Mumbai");
+            s.Add("Karnataka ", "Bangalore");
+            s.Add("Telangana", "Hyderabad");
+            s.Add("Tamilnadu ", "chennai");
+            s.Add("Bihar ", "Patna");
+            s.Add("Rajastan ", "Jaipur");
+            s.Add("Orissa ", "Bhubaneshwar");
+            Console.WriteLine("Elements in the SotredList: ");
+            foreach(var item  in s.Keys)
+            {
+                Console.WriteLine(item);
+            }
+            foreach(var item in s.Values)
+            {
+                Console.WriteLine(item);
+            }
+            IDictionaryEnumerator enumerator = s.GetEnumerator();
+            while(enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+
+        }
+
+    }
+}
---
+++ Day18/Requiremnet5/Requiremnet5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requiremnet5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Song> list = new List<Song>();
+            Console.WriteLine("Enter the number of songs");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("enter name,artist,songtype,rating ,number of downloads,date of download");
+                string input = Console.ReadLine();
+                string[] arr = input.Split(',');
+                string name = arr[0];
+                string artist = arr[1];
+                string songtype = arr[2];
+                DateTime dateDownloaded = Convert.ToDateTime(arr[3]);
+                double rating = double.Parse(arr[3]);
+                int numberofDownloads = int.Parse(arr[4]);
+               
+                Song s = new Song(name, artist, songtype,dateDownloaded, rating, numberofDownloads);
+                list.Add(s);
+
+            }
+            SortedDictionary<string, int> sd = new SortedDictionary<string, int>();
+            sd = Song.CalculateTypeCount(list);
+            Console.WriteLine("songType \t count");
+            foreach (var item  in sd)
+            {
+                Console.WriteLine(item.Key + "\t\t" + item.Value);
+            }
+        }
+    }
+}
+
