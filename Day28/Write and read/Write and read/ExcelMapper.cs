f2639bdf14d0aae2af5adbb706269a8efaf686b1
Akhil Kannuri <87297978+akhinani@users.noreply.github.com>
2023-05-17 10:05:59 +0530

    Add files via upload

---
+++ Day28/Write and read/Write and read/ExcelMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Write_and_read
+{
+    internal class ExcelMapper
+    {
+        public ExcelMapper()
+        {
+        }
+
+        internal object Fetch<T>(string v)
+        {
+            return null;
+        }
+
+        internal void Save(string newfile, List<Student> str, string v1, bool v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
