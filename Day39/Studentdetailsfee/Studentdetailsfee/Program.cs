0e8fdba74e82602e3635d4057bbd0fa0e8a69bc4
Akhil Kannuri <87297978+akhinani@users.noreply.github.com>
2023-05-24 09:16:20 +0530

    Add files via upload

---
+++ Day39/Studentdetailsfee/Studentdetailsfee/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studentdetailsfee
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the student type:");
+            string studentType = Console.ReadLine();
+            Console.WriteLine("Enter tuition fee:");
+            float tuitionFee = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter bus fee:");
+            float busFee = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter hostel fee:");
+            float hostelFee = float.Parse(Console.ReadLine());
+            float feeAmount = 0;
+            if (studentType == "MSDS")
+            {
+                feeAmount = tuitionFee + busFee;
+            }
+            else if (studentType == "MSH")
+            {
+                feeAmount = tuitionFee + hostelFee;
+            }
+            else if (studentType == "MGSDS")
+            {
+                feeAmount = 1.5f * tuitionFee + busFee;
+            }
+            else if (studentType == "MGSH")
+            {
+                feeAmount = 1.5f * tuitionFee + hostelFee;
+            }
+
+            Console.WriteLine("The fees to be paid by the student is Rs.{0}", feeAmount);
+
+        }
+    }
+}
