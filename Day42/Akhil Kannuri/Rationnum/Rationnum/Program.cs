6a03ea04fe2dcdc19ff8685bdf831b079e561c55
Akhil Kannuri <87297978+akhinani@users.noreply.github.com>
2023-05-29 09:33:29 +0530

    Add files via upload

---
+++ Day42/Akhil Kannuri/Rationnum/Rationnum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace RatioNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int pCount = 0;
+            int nCount = 0;
+            int zCount = 0;
+            foreach (int num in arr)
+            {
+                if (num > 0)
+                    pCount++;
+                else if (num < 0)
+                    nCount++;
+                else
+                    zCount++;
+
+            }
+            double pRatio = (double)pCount / n;
+            double nRatio = (double)nCount / n;
+            double zRatio = (double)zCount / n;
+            Console.WriteLine(pRatio.ToString("F6"));
+            Console.WriteLine(nRatio.ToString("F6"));
+            Console.WriteLine(zRatio.ToString("F6"));
+        }
+
+    }
+
+}
