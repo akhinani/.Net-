c849c65ac18734b0974da6f46214f0ffbb0b06fc
Akhil Kannuri <87297978+akhinani@users.noreply.github.com>
2023-06-08 17:30:52 +0530

    Add files via upload

---
+++ Day44/Biddecimal/Biddecimal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraylist
+{
+    class Program
+
+    {
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            ArrayList[] lines = new ArrayList[n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] lineData = Console.ReadLine().Split(' ');
+                int d = Convert.ToInt32(lineData[0]);
+                lines[i] = new ArrayList(d);
+                for (int j = 1; j <= d; j++)
+                {
+                    int num = Convert.ToInt32(lineData[j]);
+                    lines[i].Add(num);
+                }
+            }
+            int q = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < q; i++)
+            {
+                string[] queryData = Console.ReadLine().Split(' ');
+                int x = Convert.ToInt32(queryData[0]);
+                int y = Convert.ToInt32(queryData[1]);
+                if (x > n || y >= lines[x - 1].Count)
+                {
+                    Console.WriteLine("ERROR!");
+                }
+                else
+                {
+                    int number = (int)lines[x - 1][y];
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Day44/Prac.js
@@ -0,0 +1,12 @@
+<!DOCTYPE >
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p id="demo" style="fontSize:35px">JavaScript can change the style of an HTML element.</p>
+
+<button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">Click Me!</button>
+
+</body>
+</html> 
