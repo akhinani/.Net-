344c5131ec5632c2c9b8fb94b638e4322f6b5ef1
Akhil Kannuri <87297978+akhinani@users.noreply.github.com>
2023-06-08 17:39:36 +0530

    Add files via upload

---
+++ Day49/ProductMicroservice/ProductMicroservice/Repository/IProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using ProductMicroservice.Models;
+
+namespace ProductMicroservice.Repository
+{
+    public interface IProductRepository
+    {
+        void DeleteProduct(int id);
+        object GetProductByID(int id);
+        object GetProducts();
+        void InsertProduct(Product product);
+        void UpdateProduct(Product product);
+
+        public interface IProductRepository 
+        {
+            IEnumerable<Product> GetProducts();
+
+            object GetById(int id);
+            
+
+            Product GetProductById(int Id);
+
+            void InsertProduct(Product Product);
+
+            void DeleteProduct(Product Product);
+
+            void UpdateProduct(Product Product);
+
+            void Save();
+        }
+
+    }
+}
---
+++ Day49/ProductMicroservice/ProductMicroservice/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Configuration;
+
+using ProductMicroservice.DBContexts;
+
+using ProductMicroservice.Repository;
+
+namespace ProductMicroservice
+
+{
+
+    public class Startup
+
+    {
+
+        public Startup(ConfigurationManager configuration)
+
+        {
+
+            Configuration = configuration;
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+
+        {
+
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+
+        {
+
+            services.AddDbContext<ProductContext>(o => o.UseSqlServer(Configuration.GetConnectionString("ProductDB")));
+
+            services.AddTransient<IProductRepository, ProductRepository>();
+
+        }
+
+    }
+
+}
